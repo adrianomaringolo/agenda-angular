df301404d58c92915fcb0a30c90ac4705678b0da
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2018-05-24 16:27:12 -0300

    Lista de turmas e exclusão

--- src/app/agenda.service.ts
+++ src/app/agenda.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgendaService {
 
-  private _baseModuleURL = 'http://localhost:5000/';
+  private _restApiURL = 'http://localhost:5000/api/';
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { }
+
+  public getTurmas(): Observable<any> {
+    return this._httpClient.get(this._restApiURL + 'Turmas');
+  }
+
+  public postTurma(item: any): Observable<any> {
+    return this._httpClient.post(this._restApiURL + 'Turmas', item);
+  }
+
+  public deleteTurma(item: any): Observable<any> {
+    return this._httpClient.delete(`${this._restApiURL}Turmas/${item.id}?id=${item.id}`);
+  }
+
+  public getProfessores(): Observable<any> {
+    return this._httpClient.get(this._restApiURL + 'professores');
+  }
+
+  public getCursos(): Observable<any> {
+    return this._httpClient.get(this._restApiURL + 'cursos');
+  }
+
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,46 @@ import { AgendaService } from './agenda.service';
 })
 export class AppComponent {
 
-  constructor(private _agendaService: AgendaService) {
+  turmas: any[];
+  professores: any[];
+  cursos: any[];
 
+  constructor(private _agendaService: AgendaService) {
+    this.findTurmas();
+    this.findCursos();
+    this.findProfessores();
+  }
+
+  private findTurmas(): void {
+    this._agendaService.getTurmas().subscribe(
+      res => {
+        this.turmas = res;
+      }
+    );
+  }
+
+  private deleteTurma(turma): void {
+    this._agendaService.deleteTurma(turma).subscribe(
+      res => {
+        alert(`Turma "${turma.nome}" removida com sucesso`);
+        this.findTurmas();
+      }
+    );
+  }
+
+  private findProfessores(): void {
+    this._agendaService.getProfessores().subscribe(
+      res => {
+        this.professores = res;
+      }
+    );
+  }
+
+  private findCursos(): void {
+    this._agendaService.getCursos().subscribe(
+      res => {
+        this.cursos = res;
+      }
+    );
   }
 }
